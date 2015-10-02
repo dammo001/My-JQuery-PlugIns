d6182496d073375adf5cc6affcf18b0d4dda3ce0
David Ammons <dammo001@gmail.com>
2015-10-01 23:54:16 -0700

    add Left, Right, modify CSS to have Left, Right

--- KittenKarnage/js/kitten.js
+++ KittenKarnage/js/kitten.js
@@ -19,12 +19,21 @@
     num = (direction === 'left' ? -1 : 1); 
     event.preventDefault(); 
     this.$active.removeClass("active");
-    this.activeIdx += num; 
+    this.activeIdx += num;
+    this.setSides();  
     var $current = $("img").eq(this.activeIdx); 
     $current.addClass("active"); 
-    this.$active = $current; 
+    this.$active = $current;
+  };
 
+  $.Carousel.prototype.setSides = function() {
+    $(".carousel .left").removeClass("left");
+    $(".carousel .right").removeClass("right");  
+    var $left = $("img").eq(this.activeIdx-1);
+    var $right = $("img").eq(this.activeIdx+1); 
+    $left.addClass("left");
+    $right.addClass("right"); 
 
-  }
+    };
 
 })(jQuery);
